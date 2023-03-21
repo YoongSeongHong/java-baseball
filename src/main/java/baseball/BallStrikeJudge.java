7bdcd0bd587b100ea3086a46daa4047d06d111aa
YoongSeongHong <songhow53@naver.com>
2023-03-21 10:52:45 +0900

    강제 형변환 메소드화

--- src/main/java/baseball/BallStrikeJudge.java
+++ src/main/java/baseball/BallStrikeJudge.java
@@ -7,23 +7,32 @@ public class BallStrikeJudge {
 
     public ScoreBoard ballOrStrike(String str, List<Integer> computer){
         ScoreBoard scoreBoard = new ScoreBoard(0, 0);
+        int num = 0;
 
-        if(str.charAt(0) == (char)(computer.get(0) + '0')){
+        if(checkIfItIsStrike(str, computer, 0)){
             scoreBoard.addStrike();
-        }else if(computer.contains(Character.getNumericValue(str.charAt(0)))){
+        }else if(checkIfItIsBall(str, computer, 0)){
             scoreBoard.addBall();
         }
-        if(str.charAt(1) == (char)(computer.get(1) + '0')){
+        if(checkIfItIsStrike(str, computer, 1)){
             scoreBoard.addStrike();
-        }else if(computer.contains(Character.getNumericValue(str.charAt(1)))){
+        }else if(checkIfItIsBall(str, computer, 1)){
             scoreBoard.addBall();
         }
-        if(str.charAt(2) == (char)(computer.get(2) + '0')){
+        if(checkIfItIsStrike(str, computer, 2)){
             scoreBoard.addStrike();
-        }else if(computer.contains(Character.getNumericValue(str.charAt(2)))){
+        }else if(checkIfItIsBall(str, computer ,2)){
             scoreBoard.addBall();
         }
 
         return scoreBoard;
+    }
+
+    private static boolean checkIfItIsBall(String str, List<Integer> computer, int num) {
+        return computer.contains(Character.getNumericValue(str.charAt(num)));
+    }
+
+    private static boolean checkIfItIsStrike(String str, List<Integer> computer, int num) {
+        return str.charAt(num) == (char) (computer.get(num) + '0');
     }
 }
