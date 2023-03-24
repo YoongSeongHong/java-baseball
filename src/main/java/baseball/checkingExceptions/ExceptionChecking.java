a0ae3c2738ad2b362504a7197ebea07509e9c074
YoongSeongHong <songhow53@naver.com>
2023-03-24 19:32:19 +0900

    예외처리 메시지

--- src/main/java/baseball/checkingExceptions/ExceptionChecking.java
+++ src/main/java/baseball/checkingExceptions/ExceptionChecking.java
@@ -3,7 +3,7 @@ package baseball.checkingExceptions;
 public class ExceptionChecking {
     public String check1(String s) throws IllegalArgumentException{
         if(whetherInputOneOrTwo(s)){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1이나 2가 입력되지 않았습니다.");
         }
         return s;
     }
@@ -14,7 +14,7 @@ public class ExceptionChecking {
 
     public String check2(String s) throws IllegalArgumentException{
         if(isInputNumThreeLengthAndIntegers(s)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("세 개의 연속된 한 자리 수가 입력되지 않았습니다.");
         }
         return s;
     }
